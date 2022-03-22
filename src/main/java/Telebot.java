7340c3d4ed9fac61204ec8f1c2b9da06645aae1c
cameronmcnz <you@example.com>
2022-03-22 17:54:52 +0300

    Initial commit

---
+++ src/main/java/Telebot.java
@@ -0,0 +1,34 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Telebot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage()
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "Telebot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "BotToken";
+    }
+}
